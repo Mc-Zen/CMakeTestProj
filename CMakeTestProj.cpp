ca9fa289805923f83de0de43994316184600ace7
Mc-zen <alphabetagamma11@hotmail.com>
2021-04-15 19:20:36 +0200

    Projektdateien hinzufügen.

---
+++ CMakeTestProj.cpp
@@ -0,0 +1,12 @@
+﻿// CMakeTestProj.cpp: Definiert den Einstiegspunkt für die Anwendung.
+//
+
+#include "CMakeTestProj.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
---
+++ CMakeTestProj.h
@@ -0,0 +1,8 @@
+﻿// CMakeTestProj.h: Includedatei für Include-Standardsystemdateien
+// oder projektspezifische Includedateien.
+
+#pragma once
+
+#include <iostream>
+
+// TODO: Verweisen Sie hier auf zusätzliche Header, die Ihr Programm erfordert.
